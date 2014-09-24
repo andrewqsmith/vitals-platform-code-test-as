6848408f6ee3b1ac698e09f5ca954cc52fbd599d
Andrew Smith <andrewqsmith@hotmail.com>
2014-09-23 20:58:29 -0500

    Refractor Code
    Code complexity eased by utilizing a class.

--- award.rb
+++ award.rb
@@ -1,1 +1,53 @@
-Award = Struct.new(:name, :expires_in, :quality)
+class Award
+=begin
+Class constructed to refractor code to ease away from Booleam complexity.
+This should allow for easier reading for all programmers that may need
+to further modify this file. 
+=end
+	attr_accessor :name, :expires_in, :quality
+	
+	def initialize(name, expires_in, quality)
+	# Method to intitialize the instance variables to be passed into the class
+		@name = name
+		@expires_in = expires_in
+		@quality = quality
+	end
+	
+	def award_update!
+	# Method utilized in updating the awards according to the assigned quality and name of award
+		if (quality + award_name) <= 0
+			@quality = 0
+		elseif quality == 80
+			return
+		elseif (quality + award_name) >= 50
+			@quality = 50
+		else
+			@quality += award_name
+		end
+		@expires_in -= 1 unless name == "Blue Distinction Plus"
+	end
+
+	def award_name
+	# Utilized case to properly select award by name
+		case name
+		when "NORMAL ITEM"
+			expired? ? -2 : -1
+		when "Blue First"
+			expired? ? 2 : 1
+		when "Blue Distinction Plus"
+			0
+		when "Blue Compare"
+			if expired?
+				-1 * quality
+			elseif expires_in > 10
+				1
+			elseif expires_in > 5
+				2
+			else
+				3
+			end
+		when "Blue Star"
+			expired? ? -4 : -2
+		end
+	end
+end
--- update_quality.rb
+++ update_quality.rb
@@ -2,61 +2,7 @@ require 'award'
 
 def update_quality(awards)
 =begin
-Modified the method to include new Blue Star critieria.
-The modifications should result in -2 for each check to degrade 
-the award twice as fast. 
+File complexity reduced by utilizing a class
 =end
-  awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus' 
-          award.quality -= 1
-		  # Subtract an additional quality point for Blue Star
-		  if award.name == 'Blue Star'
-			award.quality -= 1
-		  end
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-				award.quality -= 1
-				# Add check for blue star and decrease an additional point if true
-				if	award.name == 'Blue Star'
-						award.quality -= 1
-				end
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
-  end
+  awards.each { |award| award.award_update! }
 end
